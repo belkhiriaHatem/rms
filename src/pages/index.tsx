0aa7db6f6db71f00d138cda36089fce5b7889cc2
belkhiria.hatem <belkhiria.hatem@satoripop.com>
2023-07-08 14:34:52 +0100

    bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,18 +32,18 @@ export default function Home() {
             error of his ways and repealed the joke tax.
           </p>
           <blockquote className="mt-6 border-l-2 pl-6 italic">
-            "After all," he said, "everyone enjoys a good joke, so it's only
-            fair that they should pay for the privilege."
+            After all, he said, everyone enjoys a good joke, so its only fair
+            that they should pay for the privilege.
           </blockquote>
           <div className="my-6 w-full overflow-y-auto">
             <table className="w-full">
               <thead>
                 <tr className="m-0 border-t p-0 even:bg-muted">
                   <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
-                    King's Treasury
+                    Kings Treasury
                   </th>
                   <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
-                    People's happiness
+                    Peoples happiness
                   </th>
                 </tr>
               </thead>
